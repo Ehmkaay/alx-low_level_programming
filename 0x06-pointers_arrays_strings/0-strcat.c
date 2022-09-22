fe3d3edf9f8a405e84c4ebe5aaf574e87cf8d43c
Ehmkaay <mk.agbola@gmail.com>
2022-09-22 15:43:01 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,23 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings,
+ * _strcat - Concatenates the string pointed to by @src to
+ *the end of the string pointed to by @dest
+ * @dest: String that will be appended
+ * @src: String that will be concatenated upon
  *
- * @dest: destination
- * @src: source
- * Return: the pointer to dest.
- *
+ * Return: returns pointer to @ dest *
  */
+
 char _strcat(char *dest, char src)
 {
-	int count = 0, count2 = 0;
+	int index = 0, dest_len = 0;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count 2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
